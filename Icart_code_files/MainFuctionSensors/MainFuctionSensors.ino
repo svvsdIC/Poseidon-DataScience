c92633bd89445bc81d7bf372ac67e98c83b402cf
mark <Mraehal@gmail.com>
2020-11-19 18:52:21 -0700

    Beginning of Code

---
+++ Icart_code_files/MainFuctionSensors/MainFuctionSensors.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+#include <String>
+#include <Wire.h>
+#include <time.h>
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
