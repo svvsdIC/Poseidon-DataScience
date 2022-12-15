bc86a770714a0f7009f5e332cdc81ea1726115f5
Samuel Rieger <118404789+Sam-Rieger@users.noreply.github.com>
2022-12-14 17:35:07 -0700

    Added use cases to Main

--- Test_Scripts/main.ino
+++ Test_Scripts/main.ino
@@ -1,6 +1,24 @@
 #include <Arduino.h>
 #include "sensor.h"
 
+// Sensor Use cases documented here:
+// https://docs.google.com/document/d/1oTBluc5CXEzSeSb-SR_KXQSh2Zgw1MtRs8IzuibEjN8
+
+/* TODO:
+[] low power mode - deep sleep
+[] detect sensor error condition
+[] onboard logging
+[] stream live data
+[] instruct single read
+[] Current/voltage measurement
+[] Bluetooth connection
+[] Take readings on command
+[] Measure with set delay
+[] Detect water depth
+[] Data Radio (one measure/hour)
+[] Detect Error conditions
+[] Local data storage
+*/
 
 void setup() {
     Serial.begin(9600);
