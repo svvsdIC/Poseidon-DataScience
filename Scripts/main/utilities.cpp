4fab3c8611a4d03d74b5030407e0ded0765e524c
Samuel Rieger <samueljrieger@gmail.com>
2023-09-13 19:31:31 -0600

    added function to generate CSV rows for sensorValues

--- Scripts/main/utilities.cpp
+++ Scripts/main/utilities.cpp
@@ -9,8 +9,7 @@
 
 #include <Arduino.h>
 #include "utilities.h"
-
-
+#include "sensor.h"
 
 // Takes an input in seconds and formats it into the form "hh:mm:ss"
 // with 24 hour time, then returns it to outputLocation
@@ -28,4 +27,11 @@ void formatTime(unsigned long seconds, char (&outputLocation)[MAX_TIME_CHARS + 1
 
     sprintf(outputLocation, "%02u:%02u:%02lu", hours, minutes, remainingSeconds);
 
+}
+
+// Takes a sensorvalue, returns a row for a CSV file in the format "type,timeStamp,data\n"
+void create_CSV_Row(SensorValue readingData, char (&outputStringLocation)[MAX_CSV_ROW_LENGTH + 1]) {
+    char finalCSVRow[MAX_CSV_ROW_LENGTH + 1];
+    sprintf(finalCSVRow, "%0i,%0lu,%0lf\n", (int) readingData.type, readingData.timeStamp, readingData.value);
+    strncpy(outputStringLocation, finalCSVRow, MAX_CSV_ROW_LENGTH);
 }
--- Scripts/main/utilities.h
+++ Scripts/main/utilities.h
@@ -12,9 +12,14 @@ File Description:
 // Maximum length of a formatted time string (hh:mm:ss)
 #define MAX_TIME_CHARS (8)
 
+// The maximum characters in one row of a CSV file
+#define MAX_CSV_ROW_LENGTH (40)
+
 //Takes an input in seconds and formats it into the form "hh:mm:ss"
 // then returns it to outputLocation
 void formatTime(unsigned long seconds, char (&outputLocation)[MAX_TIME_CHARS + 1]);
 
+// Takes a sensorvalue, returns a row for a CSV file in the format "type,timeStamp,data\n"
+void create_CSV_Row(SensorValue readingData, char (&outputStringLocation)[MAX_CSV_ROW_LENGTH + 1]);
 
 #endif
