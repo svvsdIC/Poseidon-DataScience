efcdfe81a37bac6df1c1769d739712b6b7634999
Samuel Rieger <samueljrieger@gmail.com>
2023-11-09 10:31:22 -0700

    Fixed incorrect array sizes for functions

--- Scripts/main/utilities.h
+++ Scripts/main/utilities.h
@@ -13,7 +13,7 @@ File Description:
 #define MAX_TIME_CHARS (8)
 
 // The maximum characters in one row of a CSV file
-#define MAX_CSV_ROW_LENGTH (256)
+#define MAX_CSV_ROW_LENGTH (32)
 
 // the header string "Electrical Conductivity,Total Dissolved Solids,Salinity,Specific Gravity,Oxygen Reduction,Dissolved Oxygen Density,Dissolved Oxygen Percent,Potential Hydrogen,Temperature\n"
 // has 171 characters.  Update MAX_CSV_ROW_LENGTH with more parameters.
@@ -28,10 +28,10 @@ void formatTime(unsigned long seconds, char (&outputLocation)[MAX_TIME_CHARS + 1
 // Takes a csv-formatted string.  Creates a unique logfile on the μsd card and writes
 // csv_header to the top of the file.  Returns 99 if writing to existing file (if
 // sensorlog99.csv already exists) or 0 if writing to a new file.  Sends file name to fileNameLocation
-int createLogFile(char csv_header[MAX_CSV_ROW_LENGTH], char (&fileNameLocation)[MAX_FILE_NAME_LENGTH]);
+int createLogFile(char csv_header[MAX_CSV_ROW_LENGTH + 1], char (&fileNameLocation)[MAX_FILE_NAME_LENGTH +1]);
 
 // takes a string of csv data and the name of a file and adds that data plus a line break
 // to the named  file.  Returns 0 for success and 99 for faliure
-int logData(char csv_data[MAX_CSV_ROW_LENGTH], char fileName[MAX_FILE_NAME_LENGTH]);
+int logData(char csv_data[MAX_CSV_ROW_LENGTH + 1], char fileName[MAX_FILE_NAME_LENGTH + 1]);
 
 #endif
